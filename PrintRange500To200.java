3a5b50d7455489b756885bbf4d5be779eeb14c58
Anchal Kushwaha <anchalkushwaha662@gmail.com>
2026-01-23 23:14:03 +0530

    Add files via upload

---
+++ PrintRange500To200.java
@@ -0,0 +1,15 @@
+// Print numbers from 500 to 200 using a while loop
+
+public class PrintRange500To200 {
+    public static void main(String[] args) {
+
+        int number = 500;
+
+        while (number >= 200) {
+            System.out.println(number);
+            number -- ;
+            // number -= 1;
+            // number -= 10;
+        }
+    }
+}
